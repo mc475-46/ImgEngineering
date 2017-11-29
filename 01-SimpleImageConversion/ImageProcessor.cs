f91e6cee0b7c01c744a61294c8d1e0a04515082e
Motoki Nagano <motoki.n.310@gmail.com>
2017-11-29 17:11:49 +0900

    課題1のファイルを追加

---
+++ 01-SimpleImageConversion/ImageProcessor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace ImageEngineering
+{
+    class ImageProcessor
+    {
+        public enum Colors { B, G, R };
+
+        public void SimpleReadBW(string srcPath, string dstPath)
+        {
+            var srcImage = Cv2.ImRead(srcPath, ImreadModes.GrayScale);
+            using (new Window(srcPath, srcImage))
+            {
+                Cv2.WaitKey();
+            }
+            Cv2.ImWrite(dstPath, srcImage);
+        }
+        public void SimpleColorChange(string srcPath, Colors color1, Colors color2, string dstPath)
+        {
+            var srcImage = Cv2.ImRead(srcPath, ImreadModes.Color);
+            var dstImage = srcImage.Clone();
+            srcImage.ExtractChannel((int)color1).InsertChannel(dstImage, (int)color2);
+            srcImage.ExtractChannel((int)color2).InsertChannel(dstImage, (int)color1);
+            using (new Window(dstPath, dstImage))
+            {
+                Cv2.WaitKey();
+            }
+            Cv2.ImWrite(dstPath, dstImage);
+        }
+        public int SimpleMix(string srcPath1, string srcPath2, double percentage, string dstPath)
+        {
+            var srcImage1 = Cv2.ImRead(srcPath1, ImreadModes.GrayScale);
+            var srcImage2 = Cv2.ImRead(srcPath2, ImreadModes.GrayScale);
+            var dstImage = srcImage1.EmptyClone();
+            for(int x = 0; x < srcImage1.Rows; ++x)
+            {
+                for(int y = 0; y < srcImage1.Cols; ++y)
+                {
+                    var srcPix1 = srcImage1.At<byte>(x, y);
+                    var srcPix2 = srcImage2.At<byte>(x, y);
+                    int dstPix = (int)(srcPix1 * percentage + srcPix2 * (1 - percentage));
+                    if (dstPix > 255) return 1;
+                    dstImage.Set<byte>(x, y, (byte)dstPix);
+                }
+            }
+            using (new Window(dstPath, dstImage))
+            {
+                Cv2.WaitKey();
+            }
+            Cv2.ImWrite(dstPath, dstImage);
+            return 0;
+        }
+        public int SimpleShift(string srcPath, int shiftValue, string dstPath)
+        {
+            var srcImage = new Mat(srcPath, ImreadModes.GrayScale);
+            var dstImage = srcImage.EmptyClone();
+            for (int x = 0; x < srcImage.Rows; ++x)
+            {
+                for (int y = 0; y < srcImage.Cols; ++y)
+                {
+                    var srcPix = srcImage.At<byte>(x, y);
+                    int dstPix;
+                    if(srcPix + shiftValue > 255)
+                    {
+                        if(srcPix - shiftValue < 0)
+                        {
+                            return 1;
+                        }
+                        dstPix = srcPix - shiftValue;
+                    }
+                    else
+                    {
+                        dstPix = srcPix + shiftValue;
+                    }
+                    dstImage.Set<byte>(x, y, (byte)dstPix);
+                }
+            }
+            using (new Window(dstPath, dstImage))
+            {
+                Cv2.WaitKey();
+            }
+            Cv2.ImWrite(dstPath, dstImage);
+            return 0;
+        }
+    }
+}
---
+++ 01-SimpleImageConversion/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEngineering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageProcessor pcs = new ImageProcessor();
+
+            Console.WriteLine("Execute SimpleReadBW");
+            Console.Write("Input source path: ");
+            string srcPath = Console.ReadLine();
+            Console.Write("Input destination path: ");
+            string dstPath = Console.ReadLine();
+            if(System.IO.File.Exists(srcPath))
+            {
+                pcs.SimpleReadBW(srcPath, dstPath);
+            }
+            else
+            {
+                Console.WriteLine($"No such file or directory: {srcPath}");
+            }
+
+            Console.WriteLine("Execute SimpleColorChange");
+            Console.Write("Input source path: ");
+            srcPath = Console.ReadLine();
+            Console.Write("Input destination path: ");
+            dstPath = Console.ReadLine();
+            Console.Write("Transcolor from (b, g, r): ");
+            string color1 = Console.ReadLine();
+            Console.Write("Transcolor to (b, g, r): ");
+            string color2 = Console.ReadLine();
+            if (System.IO.File.Exists(srcPath))
+            {
+                pcs.SimpleColorChange(srcPath, ToColors(color1), ToColors(color2), dstPath);
+            }
+            else
+            {
+                Console.WriteLine($"No such file or directory: {srcPath}");
+            }
+
+            Console.WriteLine("Execute SimpleMix");
+            Console.Write("Input mixing-base file path: ");
+            string mixBasePath = Console.ReadLine();
+            Console.Write("Input mixing file path: ");
+            string mixPath = Console.ReadLine();
+            Console.Write("Input mix percentage (0.0-1.0): ");
+            string percentageString = Console.ReadLine();
+            float percentage = 0;
+            if(!float.TryParse(percentageString, out percentage) || (percentage > 1.0 && percentage < 0.0))
+            {
+                Console.WriteLine($"Invalid Value: \"{percentageString}\", use 0 for percentage");
+            }
+            Console.Write("Input destination path: ");
+            dstPath = Console.ReadLine();
+            if (System.IO.File.Exists(mixBasePath) && System.IO.File.Exists(mixPath))
+            {
+                if (pcs.SimpleMix(mixBasePath, mixPath, percentage, dstPath) != 0)
+                {
+                    Console.WriteLine("Failed to mix");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No such file or directory: {mixBasePath} or {mixPath}");
+            }
+
+            Console.WriteLine("Execute SimpleShift");
+            Console.Write("Input source path: ");
+            srcPath = Console.ReadLine();
+            Console.Write("Input destination path: ");
+            dstPath = Console.ReadLine();
+            Console.Write("Input shift value(>=0): ");
+            string shiftString = Console.ReadLine();
+            int shiftValue = 0;
+            if (!int.TryParse(shiftString, out shiftValue))
+            {
+                Console.WriteLine($"Invalid Value: \"{shiftString}\", use 0 for shift value");
+            }
+            if (pcs.SimpleShift(srcPath, shiftValue, dstPath) != 0)
+            {
+                Console.WriteLine("Failed to shift");
+            }
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.Read();
+        }
+
+        static ImageProcessor.Colors ToColors(string colorString)
+        {
+            switch (colorString[0])
+            {
+                case 'b':
+                    return ImageProcessor.Colors.B;
+                case 'g':
+                    return ImageProcessor.Colors.G;
+                default:
+                    return ImageProcessor.Colors.R;
+            }
+        }
+    }
+}
